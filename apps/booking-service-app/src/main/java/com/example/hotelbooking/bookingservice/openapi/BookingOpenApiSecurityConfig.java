6a252cb1f3c23d54337a1ee0e541c48251804d09
Volkov Sergei <volkovself@gmail.com>
2026-05-03 22:02:56 +0300

    feat(api): add JWT bearer auth to OpenAPI

--- apps/booking-service-app/src/main/java/com/example/hotelbooking/bookingservice/openapi/BookingOpenApiSecurityConfig.java
+++ apps/booking-service-app/src/main/java/com/example/hotelbooking/bookingservice/openapi/BookingOpenApiSecurityConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,8 @@ import org.springframework.context.annotation.Configuration;
         @Info(
             title = "Booking Service API",
             version = "v0.5.0",
-            description = "Booking service API with JWT bearer authentication support."))
+            description = "Booking service API with JWT bearer authentication support."),
+    security = @SecurityRequirement(name = "bearerAuth"))
 @SecurityScheme(
     name = "bearerAuth",
     type = SecuritySchemeType.HTTP,
